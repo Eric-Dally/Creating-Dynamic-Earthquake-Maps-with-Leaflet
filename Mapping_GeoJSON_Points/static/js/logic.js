32c8e39f28b8d585fcbcce8bb8dcdf87ee8af162
Eric Dally <82424250+Eric-Dally@users.noreply.github.com>
2021-07-14 10:42:11 -0500

    adding lightand dark layers

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,5 +1,5 @@
-// Create the map object with center and zoom level.
-let map = L.map('mapid').setView([30, 30], 2);
+// // Create the map object with center and zoom level.
+// let map = L.map('mapid').setView([30, 30], 2);
 
 // // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -9,7 +9,7 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 
 // // Then we add our 'graymap' tile layer to the map.
-streets.addTo(map);
+// streets.addTo(map);
 
 
 // Add GeoJSON data.
@@ -70,35 +70,65 @@ let sanFranAirport =
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/Eric-Dally/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
 
+// // Skill Drill: Grabbing our GeoJSON data.
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+// L.geoJson(data, {
+
+// //Skill Drill: Trying the onEachFeature function to creat popup markers:
+//   onEachFeature: function(feature, layer) {
+//     console.log(layer);
+//     layer.bindPopup("<h2> Airport code: " + feature.properties.faa +
+//     "</h2> <hr> <h3> Airport name: " + feature.properties.name + "</h3>");
+//     }
+//   }).addTo(map);
+// })
+
+
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// //Orginal code:
 // Grabbing our GeoJSON data.
 d3.json(airportData).then(function(data) {
   console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
+  L.geoJson(data).addTo(map);
+  });
 
-//Skill Drill: Trying the onEachFeature function to creat popup markers:
-  onEachFeature: function(feature, layer) {
-    console.log(layer);
-    layer.bindPopup("<h2> Airport code: " + feature.properties.faa +
-    "</h2> <hr> <h3> Airport name: " + feature.properties.name + "</h3>");
-    }
-  }).addTo(map);
-})
+//Skill Drill: Edit Edit your L.geoJson() layer to add a popup marker
+// that displays all airports' codes and names for both the Street and Dark layers.
 
-
-// for (var i = 0; i < airportData.length; i++) {
-//   var jdata = airportData[i]
-//   L.marker(jdata.coordinates)
-//     .bindPopup("<h2> Airport code: " + jdata.faa + 
-//     "</h2> <hr> <h3> Airport name: " + jdata.name + "</h3>")
-//     .addTo(map);
-// }});
-
-//Original Code:
 // Grabbing our GeoJSON data.
-//d3.json(airportData).then(function(data) {
-  //console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-// L.geoJson(data).addTo(map);
-// });
-
+// d3.json(airportData).then(function(data) {
+//   console.log(data);
+// // Creating a GeoJSON layer with the retrieved data.
+//   L.geoJson(data, {
+//     onEachFeature: function(feature, layer) {
+//           console.log(layer);
+//           layer.bindPopup("<h2> Airport code: " + feature.properties.faa +
+//           "</h2> <hr> <h3> Airport name: " + feature.properties.name + "</h3>");
+//           }
+//         }).addTo(map);
+//       });
